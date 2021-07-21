053b3e030150c6cd8d494f565715adf94ad03017
Tammy <tammycwong@gmail.com>
2021-07-21 19:52:50 -0400

    create board

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,2 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const cardArray = [
+        {
+            name: 'Dot',
+            img: 'images/dot.png',
+        },
+        {
+            name: 'Lickitung',
+            img: 'images/lickitung.png',
+        },
+        {
+            name: 'Porkchop',
+            img: 'images/porkchop.png',
+        },
+        {
+            name: 'Rocco',
+            img: 'images/rocco.png',
+        },
+        {
+            name: 'Simba',
+            img: 'images/simba.png',
+        },
+        {
+            name: 'Snorlax',
+            img: 'images/snorlax.png',
+        },
+        {
+            name: 'Squidward',
+            img: 'images/Squidward.png',
+        }
+    ]
+
+    const grid = document.querySelector('.grid')
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/cassette.png')
+            card.setAttribute('id', i)
+            grid.appendChild(card)
+        }
+    }
+    createBoard();
 })
